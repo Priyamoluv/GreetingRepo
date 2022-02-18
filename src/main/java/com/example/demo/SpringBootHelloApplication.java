207aa4c522a25183157911466bd8cca3c85e0c00
Priya <priyamoluv0326@gmail.com>
2022-02-18 11:14:31 +0530

    First commit

--- src/main/java/com/example/demo/SpringBootHelloApplication.java
+++ src/main/java/com/example/demo/SpringBootHelloApplication.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @EnableAutoConfiguration
 public class SpringBootHelloApplication {
     
-	@RequestMapping("/greetings")
+	@RequestMapping("/")
 	@ResponseBody
 	String firstPage() {
 		return "Hello World";
